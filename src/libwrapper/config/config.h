4951d6d22b512ca9e3cbb3de2c4bd1ab79ce3a0d
RicardoRagel <rragde@upo.es>
2021-09-10 13:01:06 +0200

    Added example msg type and topic

--- src/libwrapper/config/config.h
+++ src/libwrapper/config/config.h
@@ -8,3 +8,7 @@
 *
 */
 
+// This is an example topic to be send from the roscore on 192.168.1.1 to 
+// the roscore on 192.168.1.2, assuming you have not modified the base_ip
+// in the config YAML file. This line must be in both remote ros_pound nodes
+TOPIC(std_msgs::String, "chatter", 1, "2", 100, 5, 10000)
