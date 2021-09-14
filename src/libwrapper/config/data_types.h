76522fe1fca2076bd5c0a6bc03db2ffd85ad8850
RicardoRagel <rragde@upo.es>
2021-09-14 09:25:53 +0200

    Fixing comments on the example chatter topic

--- src/libwrapper/config/data_types.h
+++ src/libwrapper/config/data_types.h
@@ -1,5 +1,5 @@
 // This is the msg include for the example topic to be send from the roscore 
-// on 192.168.1.1 to the roscore on 192.168.1.2, assuming you have not modified 
+// on 192.168.1.2 to the roscore on 192.168.1.3, assuming you have not modified 
 // the base_ip in the config YAML file. This line must be in both remote ros_pound nodes
 #include <std_msgs/String.h>
 
